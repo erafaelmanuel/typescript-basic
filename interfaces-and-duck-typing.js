5a2990829cbc3f83c25c12d200065e221150fea5
erafaelmanuel <erafaelmanuel@gmail.com>
2018-03-18 23:17:38 +0800

    Interfaces and duck typing

---
+++ interfaces-and-duck-typing.js
@@ -0,0 +1,21 @@
+var Programmer = /** @class */ (function () {
+    function Programmer() {
+    }
+    Programmer.prototype.doStuff = function () {
+        return "foo";
+    };
+    Programmer.prototype["break"] = function () {
+        console.log("bar");
+    };
+    return Programmer;
+}());
+var eagers = [];
+eagers.push(new Programmer());
+eagers.push({
+    doStuff: function () {
+        return "foo";
+    },
+    "break": function () {
+        console.log("bar");
+    }
+});
--- interfaces-and-duck-typing.ts
+++ interfaces-and-duck-typing.ts
@@ -3,3 +3,25 @@ interface Eager {
     doStuff(): string;
     break(): void;
 }
+
+class Programmer implements Eager {
+    doStuff() {
+        return "foo"
+    }
+
+    break() {
+        console.log("bar")
+    }
+}
+
+var eagers: Eager[] = [];
+
+eagers.push(new Programmer())
+eagers.push({
+    doStuff() {
+        return "foo"
+    },
+    break() {
+        console.log("bar")
+    }
+})
