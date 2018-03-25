c72670fd651bf8028a883fc23a9dea18bca2c7a4
erafaelmanuel <erafaelmanuel@gmail.com>
2018-03-25 23:23:54 +0800

    Wrapping up

--- practice/out/index.js
+++ practice/out/index.js
@@ -1,10 +1,28 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-var github_service_1 = require("./service/github.service");
-var gv = new github_service_1.GithubService();
-gv.getUserInfo("erafaelmanuel", function (user) {
-    console.log(user);
-});
-gv.getRepos("erafaelmanuel", function (repos) {
-    console.log(repos);
-});
+var _ = __importStar(require("lodash"));
+var githubService_1 = require("./service/githubService");
+(function init() {
+    if (process.argv.length < 3) {
+        console.log("Please pass the username as an argument");
+    }
+    else {
+        var username = process.argv[2];
+        var gv = new githubService_1.GithubService();
+        gv.getUserInfo(username, function (user) {
+            gv.getRepos(username, function (repos) {
+                var sorted = _.sortBy(repos, function (repo) { return repo.forkCount * -1; });
+                var filtered = _.take(sorted, 6);
+                user.repos = filtered;
+                console.log(user);
+            });
+        });
+    }
+})();
--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,13 +1,23 @@
-import {GithubService} from "./service/github.service"
+import * as _ from "lodash"
+import {GithubService} from "./service/githubService"
 import { User } from "./domain/user";
 import { Repo } from "./domain/repo";
 
-var gv = new GithubService()
+(function init() {
+    if (process.argv.length < 3) {
+        console.log("Please pass the username as an argument")
+    } else {
+        var username = process.argv[2]
+        var gv = new GithubService()
+        gv.getUserInfo(username, (user: User) => {
+            gv.getRepos(username, (repos: Repo[]) => {
+                var sorted = _.sortBy(repos, (repo: Repo) => repo.forkCount * -1)
+                var filtered = _.take(sorted, 6)
+                user.repos = filtered
+                console.log(user)
+            })
+        })
+    }
+})()
 
-gv.getUserInfo("erafaelmanuel", (user: User) => {
-    console.log(user)
-})
 
-gv.getRepos("erafaelmanuel", (repos: Repo[]) => {
-    console.log(repos)
-})
